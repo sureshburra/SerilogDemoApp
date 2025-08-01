a9c15d50acc40a227e2954fa90d7bca39f86f5f6
suresh.burra <suresh.burra@hexagon.com>
2025-08-01 16:24:49 +0530

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SerilogDemoApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        _logger.LogInformation("Index page loaded");
+
+        try
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 56)
+                {
+                    throw new Exception("This is our demo exception");
+                }
+                else
+                {
+                    _logger.LogInformation("Processing item {iVariable}", i);
+                }
+            }
+
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while processing the request");
+        }
+    }
+}
